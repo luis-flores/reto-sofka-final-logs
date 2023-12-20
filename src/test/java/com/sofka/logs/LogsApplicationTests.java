36313d2d1a61f66fa0f360328f333dc8e2507084
Luis Diego Flores Villalobos <flores.luisdiego@gmail.com>
2023-12-20 16:55:00 -0600

    Avance de kubernetes
    Avance de configuración para kubernetes

--- src/test/java/com/sofka/logs/LogsApplicationTests.java
+++ src/test/java/com/sofka/logs/LogsApplicationTests.java
@@ -3,6 +3,7 @@ package com.sofka.logs;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
 @SpringBootTest
 class LogsApplicationTests {
 
@@ -11,3 +12,4 @@ class LogsApplicationTests {
 	}
 
 }
+*/
